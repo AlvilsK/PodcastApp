914fc5711f933b82fb7760c20a7db38db2025947
alvilsk <ttepatt@gmail.com>
2016-05-08 12:51:17 +0000

    Permit attributes for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :configuere_permitted_parameters, if: :devise_controller?
+  
+  protected
+  
+  def configuere_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay) }
+  end
+
+  def configuere_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay) }
+  end
+  
+  
 end

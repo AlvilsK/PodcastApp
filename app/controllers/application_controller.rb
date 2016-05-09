3d8deb39a14c0f2e1c0a6ee3de887a2730aaeb76
alvilsk <ttepatt@gmail.com>
2016-05-09 20:15:29 +0000

    Fix permisions in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,13 @@ class ApplicationController < ActionController::Base
   protected
   
   def configuere_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay) }
+  end  
+  
+  def configuere_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay) }
   end
 
-  def configuere_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :title, :description, :itunes, :stitcher, :podbay) }
-  end
   
   
 end
